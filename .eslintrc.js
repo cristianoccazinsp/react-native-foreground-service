c27f35a6d7384a54d0cd7fb680881662dc75bb00
Cristiano Coelho <cristianocca@hotmail.com>
2021-04-28 13:14:36 -0300

    add eslint config and fix styles

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  settings: {
+    react: {
+      version: '16',
+    },
+  },
+  rules: {
+    'react/no-did-mount-set-state': 0,
+    'react/no-did-update-set-state': 0,
+    eqeqeq: 0,
+    'react-native/no-inline-styles': 0,
+    'no-catch-shadow': 0,
+    'no-sequences': 2,
+    'handle-callback-err': 0,
+    'no-useless-escape': 0,
+    radix: 0,
+    'no-shadow': 0,
+    'no-func-assign': 0,
+  },
+};
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import {NativeModules, AppRegistry} from 'react-native';
 
 // ANDROID ONLY
@@ -18,126 +17,122 @@ const ForegroundServiceModule = NativeModules.ForegroundService;
  * @property {boolean} ongoing - true/false if the notification is ongoing. The notification the service was started with will always be ongoing
  * @property {number} [importance] - Importance (and priority for older devices) of this notification. This might affect notification sound One of:
  *                                  none - IMPORTANCE_NONE (by default),
-    *                               min - IMPORTANCE_MIN,
-    *                               low - IMPORTANCE_LOW,
-    *                               default - IMPORTANCE_DEFAULT
-    *                               high - IMPORTANCE_HIGH,
-    *                               max - IMPORTANCE_MAX
+ *                               min - IMPORTANCE_MIN,
+ *                               low - IMPORTANCE_LOW,
+ *                               default - IMPORTANCE_DEFAULT
+ *                               high - IMPORTANCE_HIGH,
+ *                               max - IMPORTANCE_MAX
  */
-const NotificationConfig = {
-
-};
+// eslint-disable-next-line no-unused-vars
+const NotificationConfig = {};
 
 /**
  * @property {string} taskName - name of the js task configured with registerForegroundTask
  * @property {number} delay - start task in delay miliseconds, use 0 to start immediately
  * ... any other values passed to the task as well
  */
-const TaskConfig = {
-
-}
-
+// eslint-disable-next-line no-unused-vars
+const TaskConfig = {};
 
 export default class ForegroundService {
+  /**
+   * Registers a piece of JS code to be ran on the service
+   * NOTE: This must be called before anything else, or the service will fail.
+   * NOTE2: Registration must also happen at module level (not at mount)
+   * task will receive all parameters from runTask
+   * @param {string} taskName task name that should match
+   * @param {function} task async function to be called
+   */
+  static registerForegroundTask(taskName, task) {
+    AppRegistry.registerHeadlessTask(taskName, () => task);
+  }
 
-    /**
-     * Registers a piece of JS code to be ran on the service
-     * NOTE: This must be called before anything else, or the service will fail.
-     * NOTE2: Registration must also happen at module level (not at mount)
-     * task will receive all parameters from runTask
-     * @param {string} taskName task name that should match
-     * @param {function} task async function to be called
-     */
-    static registerForegroundTask(taskName, task) {
-        AppRegistry.registerHeadlessTask(taskName, () => task);
-    }
+  /**
+   * Start foreground service
+   * Multiple calls won't start multiple instances of the service, but will increase its internal counter
+   * so calls to stop won't stop until it reaches 0.
+   * Note: notificationConfig can't be re-used (becomes immutable)
+   * @param {NotificationConfig} notificationConfig - Notification config
+   * @return Promise
+   */
+  static async startService(notificationConfig) {
+    return await ForegroundServiceModule.startService(notificationConfig);
+  }
 
-    /**
-     * Start foreground service
-     * Multiple calls won't start multiple instances of the service, but will increase its internal counter
-     * so calls to stop won't stop until it reaches 0.
-     * Note: notificationConfig can't be re-used (becomes immutable)
-     * @param {NotificationConfig} notificationConfig - Notification config
-     * @return Promise
-     */
-    static async startService(notificationConfig) {
-        return await ForegroundServiceModule.startService(notificationConfig);
-    }
+  /**
+   * Updates a notification of a running service. Make sure to use the same ID
+   * or it will trigger a separate notification.
+   * Note: this method might fail if called right after starting the service
+   * since the service might not be yet ready.
+   * If service is not running, it will be started automatically like calling startService.
+   * @param {NotificationConfig} notificationConfig - Notification config
+   * @return Promise
+   */
+  static async updateNotification(notificationConfig) {
+    return await ForegroundServiceModule.updateNotification(notificationConfig);
+  }
 
-    /**
-     * Updates a notification of a running service. Make sure to use the same ID
-     * or it will trigger a separate notification.
-     * Note: this method might fail if called right after starting the service
-     * since the service might not be yet ready.
-     * If service is not running, it will be started automatically like calling startService.
-     * @param {NotificationConfig} notificationConfig - Notification config
-     * @return Promise
-     */
-    static async updateNotification(notificationConfig) {
-        return await ForegroundServiceModule.updateNotification(notificationConfig);
-    }
+  /**
+   * Cancels/dimisses a notification given its id. Useful if the service used
+   * more than one notification
+   * @param {number} id - Notification id to cancel
+   * @return Promise
+   */
+  static async cancelNotification(id) {
+    return await ForegroundServiceModule.cancelNotification({id: id});
+  }
 
-    /**
-     * Cancels/dimisses a notification given its id. Useful if the service used
-     * more than one notification
-     * @param {number} id - Notification id to cancel
-     * @return Promise
-     */
-    static async cancelNotification(id) {
-        return await ForegroundServiceModule.cancelNotification({id: id});
-    }
+  /**
+   * Stop foreground service. Note: Pending tasks might still complete.
+   * If startService will called multiple times, this needs to be called as many times.
+   * @return Promise
+   */
+  static async stopService() {
+    return await ForegroundServiceModule.stopService();
+  }
 
-    /**
-     * Stop foreground service. Note: Pending tasks might still complete.
-     * If startService will called multiple times, this needs to be called as many times.
-     * @return Promise
-     */
-    static async stopService() {
-        return await ForegroundServiceModule.stopService();
-    }
+  /**
+   * Stop foreground service. Note: Pending tasks might still complete.
+   * This will stop the service regardless of how many times start was called
+   * @return Promise
+   */
+  static async stopServiceAll() {
+    return await ForegroundServiceModule.stopServiceAll();
+  }
 
-    /**
-     * Stop foreground service. Note: Pending tasks might still complete.
-     * This will stop the service regardless of how many times start was called
-     * @return Promise
-     */
-    static async stopServiceAll() {
-        return await ForegroundServiceModule.stopServiceAll();
-    }
+  /**
+   * Runs a previously configured headless task.
+   * Task must be able to self stop if the service is stopped, since it can't be force killed once started.
+   * Note: This method might silently fail if the service is not running, but will run successfully
+   * if the service is still spinning up.
+   * If the service is not running because it was killed, it will be attempted to be started again
+   * using the last notification available.
+   * @param {TaskConfig} taskConfig - Notification config
+   * @return Promise
+   */
+  static async runTask(taskConfig) {
+    return await ForegroundServiceModule.runTask(taskConfig);
+  }
 
-    /**
-     * Runs a previously configured headless task.
-     * Task must be able to self stop if the service is stopped, since it can't be force killed once started.
-     * Note: This method might silently fail if the service is not running, but will run successfully
-     * if the service is still spinning up.
-     * If the service is not running because it was killed, it will be attempted to be started again
-     * using the last notification available.
-     * @param {TaskConfig} taskConfig - Notification config
-     * @return Promise
-     */
-    static async runTask(taskConfig) {
-        return await ForegroundServiceModule.runTask(taskConfig);
-    }
+  /**
+   * Returns an integer indicating if the service is running or not.
+   * The integer represents the internal counter of how many startService
+   * calls were done without calling stopService
+   * @return Promise
+   */
+  static async isRunning() {
+    return await ForegroundServiceModule.isRunning();
+  }
 
-    /**
-     * Returns an integer indicating if the service is running or not.
-     * The integer represents the internal counter of how many startService
-     * calls were done without calling stopService
-     * @return Promise
-     */
-    static async isRunning() {
-        return await ForegroundServiceModule.isRunning();
-    }
-
-    /**
-     * Returns true if background is restricted as provided by
-     * https://developer.android.com/reference/android/app/ActivityManager#isBackgroundRestricted()
-     *
-     * Returns false if restriction cannot be determined due to Android constraints,
-     * only available for SDK >= 28
-     * @return Promise
-     */
-     static async isBackgroundRestricted() {
-        return await ForegroundServiceModule.isBackgroundRestricted();
-    }
+  /**
+   * Returns true if background is restricted as provided by
+   * https://developer.android.com/reference/android/app/ActivityManager#isBackgroundRestricted()
+   *
+   * Returns false if restriction cannot be determined due to Android constraints,
+   * only available for SDK >= 28
+   * @return Promise
+   */
+  static async isBackgroundRestricted() {
+    return await ForegroundServiceModule.isBackgroundRestricted();
+  }
 }
